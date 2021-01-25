2ed34f77353c6534ebe5afe70eca0f4cac5e487d
Lucas de Miranda Lima <60715381+LucasLima939@users.noreply.github.com>
2021-01-25 12:31:16 -0300

    Update ReadMe.ml

--- ReadMe.ml
+++ ReadMe.ml
@@ -3,4 +3,4 @@ e banco de dados SQL para realização de CRUD. É realizado o cadastro, pesquis
 atualização de dados, exclusão e armazenamento no BD.
 
 This is an employees management system made in Java with XHTML and SQL Database, using the CRUD logic.
-What it does
+What it does is cadastrate, search, update, delete and store in DB.
