ff63453c67cdcd39d2f71e9c5d73bffa655c8464
Lucas de Miranda Lima <60715381+LucasLima939@users.noreply.github.com>
2021-01-25 09:29:21 -0300

    Create ReadMe.ml

---
+++ ReadMe.ml
@@ -0,0 +1,6 @@
+Este é um programa de gerenciamente de funcionários em Java utilizando XHTML
+e banco de dados SQL para realização de CRUD. É realizado o cadastro, pesquisa,
+atualização de dados, exclusão e armazenamento no BD.
+
+This is an employees management system made in Java with XHTML and SQL Database, using the CRUD logic.
+What it does
